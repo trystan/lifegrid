30df5a09153b2a521070b8602b7c5296b488cfbf
Trystan Spangler <trystan.s@comcast.net>
2013-05-27 15:29:41 -0700

    plants have several "points" they can allocate to growth rather than just one number

--- plant.rb
+++ plant.rb
@@ -1,9 +1,9 @@
 
 class Plant
-  attr_reader :x, :y, :color, :preferred_climate
+  attr_reader :x, :y, :color, :growth
 
   def alive?
-    @energy > 0 && @age < 25
+    @energy > 0 && @age < 100
   end
 
   def occupied_map
@@ -15,7 +15,7 @@ class Plant
       @x = [[0, parent.x + rand(11) - 5].max, 199].min
       @y = [[0, parent.y + rand(11) - 5].max, 199].min
       @color = parent.color.clone
-      @preferred_climate = parent.preferred_climate
+      @growth = parent.growth.clone
       @energy = 30
       @age = 1
       mutate
@@ -23,7 +23,8 @@ class Plant
       @x = rand(200)
       @y = rand(200)
       @color = [rand(64)+32, rand(64)+32, rand(64)+32]
-      @preferred_climate = rand(9)
+      @growth = Array.new(9) { 0 }
+      9.times.each { @growth[rand(9)] += 1 }
       @energy = rand(50) + 25
       @age = rand(25)
       occupied_map[x][y] = true
@@ -33,23 +34,22 @@ class Plant
   end
 
   def mutate
-    @color[0] = [[32, @color[0] + rand(7) - 3].max, 64+32].min
-    @color[1] = [[32, @color[1] + rand(7) - 3].max, 64+32].min
-    @color[2] = [[32, @color[2] + rand(7) - 3].max, 64+32].min
+    @color[0] = [[32, @color[0] + rand(11) - 5].max, 64+32].min
+    @color[1] = [[32, @color[1] + rand(11) - 5].max, 64+32].min
+    @color[2] = [[32, @color[2] + rand(11) - 5].max, 64+32].min
 
-    if rand(3) == 1
-      @preferred_climate = rand(3) - 1
+    from = rand(9)
+    to = rand(9)
+    if @growth[from] > 0 && @growth[to] < 9
+      @growth[from] -= 1
+      @growth[to] += 1
     end
   end
 
   def update
     @age += 1
 
-    if @climate_map[x, y] == preferred_climate
-      @energy += 10
-    else
-      @energy -= 10
-    end
+    @energy += growth[@climate_map[x, y]] - 1
 
     if @energy >= 100
       birth
